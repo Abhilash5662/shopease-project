8e70ccad6eb86246c24c6406922bce171f368e63
Abhilash5662 <abhilashsivaram127@gmail.com>
2026-03-16 14:39:09 +0530

    updated requirements for deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*f=rzbo$7qwxwhdiwpb5mz1v8ge)5@g)-bt%u%^a^2$0rjh@k2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -123,6 +123,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 import os
 
--- backend/store/models.py
+++ backend/store/models.py
@@ -8,6 +8,7 @@ class Category(models.Model):
         return self.name
 
 
+
 class Product(models.Model):
     title = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
