6a32fe3f5510a99573c22aff9aa7fbf370026586
Abhilash5662 <abhilashsivaram127@gmail.com>
2026-03-16 14:54:01 +0530

    Fixed missing os import

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,6 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
